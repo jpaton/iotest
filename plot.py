a3a4ccbcefb13ac234e72eda51a4d71f7ab22bd8
James Paton <paton@cs.wisc.edu>
2012-05-09 15:26:36 -0500

    apparently didn't stage plot.py last time

--- plot.py
+++ plot.py
@@ -6,19 +6,19 @@ reader = csv.DictReader(open(sys.argv[1], 'r'), fieldnames = ['block', 'time'])
 
 data = sorted([(int(row['block']), int(row['time'])) for row  in reader])
 
-print "\n".join(map(str, data[:100]))
-
 if len(sys.argv) > 2:
 	maxBlock = int(sys.argv[2])
 else:
 	maxBlock = len(data)
 
-subset = data[:maxBlock]
+subset = list(zip(*data[:maxBlock])[1])
+print subset[:20]
 mean = np.mean(subset)
 stddev = np.std(subset)
 print "N: %d" % len(subset)
 print "Mean: %f" % mean
 print "Std. dev.: %f" % stddev
 print "Std. dev. compared to mean: %f" % (stddev / mean)
-plt.plot(zip(*subset)[1])
+print "Auto correlation: " + str(np.correlate(subset, subset, mode='same'))
+plt.plot(subset)
 plt.show()
