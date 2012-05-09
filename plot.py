0ca7d72f39dac4335e9823219e8c3b7d93f3a588
James Paton <paton@cs.wisc.edu>
2012-05-09 15:33:41 -0500

    removed autocorrelation altogether

--- plot.py
+++ plot.py
@@ -11,7 +11,7 @@ if len(sys.argv) > 2:
 else:
 	maxBlock = len(data)
 
-subset = list(zip(*data[:maxBlock])[1])
+subset = zip(*data[:maxBlock])[1]
 print subset[:20]
 mean = np.mean(subset)
 stddev = np.std(subset)
@@ -19,6 +19,5 @@ print "N: %d" % len(subset)
 print "Mean: %f" % mean
 print "Std. dev.: %f" % stddev
 print "Std. dev. compared to mean: %f" % (stddev / mean)
-print "Auto correlation: " + str(np.correlate(subset, subset, mode='same'))
 plt.plot(subset)
 plt.show()
